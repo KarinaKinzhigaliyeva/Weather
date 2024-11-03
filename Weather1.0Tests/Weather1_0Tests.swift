cdd4ca304567073ff518fc219752d389b3dfb37e
Karina Kinzhigaliyeva <karinakinzhigaliyeva@gmail.com>
2024-11-03 22:18:07 +0500

    Initial Commit

---
+++ Weather1.0Tests/Weather1_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather1_0Tests.swift
+//  Weather1.0Tests
+//
+//  Created by Karina Kinzhigaliyeva on 03.11.2024.
+//
+
+import Testing
+@testable import Weather1_0
+
+struct Weather1_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
